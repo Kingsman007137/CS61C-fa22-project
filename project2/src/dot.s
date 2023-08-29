a39e5376cf37093c9a43fad47247594d4e319257
pan <3398530682@qq.com>
2023-08-29 16:41:13 +0800

    finish part a task 4

--- project2/src/dot.s
+++ project2/src/dot.s
@@ -20,23 +20,43 @@
 dot:
 
     # Prologue
-
-
+check_num:
+    addi t0, x0, 1
+    bge a2, t0, check_stride
+    li a0, 36
+    j exit
+check_stride:
+    blt a3, t0, error
+    bge a4, t0, loop_start
+error:
+    li a0, 37
+    j exit
 loop_start:
+    #t0 store the sum
+    add t0, x0, x0
+    add t1, x0, a2
+loop_continue:
+ebreak
+    #get the t2th value of the first array
+    sub t2, a2, t1
+    mul t2, t2, a3
+    slli t2, t2, 2
+    add t2, t2, a0
+    lw t4, 0(t2)
+    #the second array is almost the same
+    sub t2, a2, t1
+    mul t2, t2, a4
+    slli t2, t2, 2
+    add t2, t2, a1
+    lw t5, 0(t2)
 
-
-
-
-
-
-
-
-
-
-
-
+    mul t4, t4, t5
+    add t0, t0, t4
 loop_end:
-
+    addi t1, t1, -1
+    addi t2, x0, 1
+    bge t1, t2, loop_continue
+    mv a0, t0
 
     # Epilogue
 
