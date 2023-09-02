449b05dd24bd1c7b19524d68d730964f443e133c
pan <3398530682@qq.com>
2023-09-02 23:02:31 +0800

    finish task 8

--- project2/src/write_matrix.s
+++ project2/src/write_matrix.s
@@ -25,16 +25,120 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -8
+    sw s0, 0(sp)
+    sw ra, 4(sp)
+    
+
+    # step 1
+    ebreak
+    addi sp, sp, -12
+    sw a1, 0(sp)
+    sw a2, 4(sp)
+    sw a3, 8(sp)
+
+    addi a1, x0, 1
+    jal fopen
+    addi t0, x0, -1
+    beq a0, t0, fopen_error
+
+    lw a1, 0(sp)
+    lw a2, 4(sp)
+    lw a3, 8(sp)
+    addi sp, sp, 12
 
 
+    # step 2
+    addi sp, sp, -16
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
+
+    addi a0, x0, 8
+    jal malloc
+    add t0, a0, x0
+
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    addi sp, sp, 16
+
+    # call fwrite to write the num of rows and cols
+    addi sp, sp, -20
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
+    sw t0, 16(sp)
+
+    sw a2, 0(t0)
+    sw a3, 4(t0)
+    mv a1, t0
+    addi a2, x0, 2
+    addi a3, x0, 4
+    jal fwrite
+    addi a2, x0, 2
+    bne a0, a2, fwrite_error
+
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    lw t0, 16(sp)
+    addi sp, sp, 20
 
 
+    # step 3
+    addi sp, sp, -16
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
+
+    mul s0, a2, a3
+    mv a2, s0
+    addi a3, x0, 4
+    jal fwrite
+    bne a0, s0, fwrite_error
+
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    addi sp, sp, 16
 
 
+    # step 4
+    addi sp, sp, -16
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
 
+    jal fclose
+    bne a0, x0, fclose_error
 
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    addi sp, sp, 16
 
-    # Epilogue
-
+    # Epilogue    
+    lw s0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
 
     jr ra
+
+fopen_error:
+    li a0, 27
+    j exit
+fclose_error:
+    li a0, 28
+    j exit
+fwrite_error:
+    li a0, 30
+    j exit
